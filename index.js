9fb92f0881688aad45e357728aa430111774bc9f
AshwinB-hat <xendergrunge@gmail.com>
2017-12-15 14:54:21 +0300

    Initial Stage

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+var ejs = require("ejs");
+var path = require("path")
+
+app.use(express.static(path.join(__dirname, 'views')));
+app.set("view engine", "ejs");
+
+app.get("/",function(req,res){
+	res.render("index");
+});
+
+app.listen(3000,function(req,res){
+	console.log("began listening");
+});
